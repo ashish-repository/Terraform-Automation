f114e3d65919ffcbd5a80e525d4952f4320a8cac
ashish <ashishsaharelearning@gmail.com>
2025-06-04 23:25:57 +0530

    update jenkins file

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block       = "172.16.0.0/16"
   instance_tenancy = "default"
   tags = {
     Name = "main"
@@ -22,14 +22,14 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- # outbound from jenkis server
+  # outbound from jenkis server
   egress {
     from_port   = 0
     to_port     = 65535
@@ -37,17 +37,17 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = var.security_group
   }
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
+  tags = {
     Name = var.tag_name
   }
 }
@@ -56,7 +56,7 @@ resource "aws_instance" "myFirstInstance" {
 resource "aws_eip" "myFirstInstance" {
   vpc      = true
   instance = aws_instance.myFirstInstance.id
-tags= {
+  tags = {
     Name = "my_elastic_ip"
   }
 }
